cee9fd40c1aafbf28ffe7f644af891b582d5aab9
Saee25 <saeenalawade2545@gmail.com>
2025-11-28 23:42:23 +0530

    pushed files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,19 @@
-export default function App() {
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import MsmeDashboard from './pages/MsmeDashboard';
+import LenderDashboard from './pages/LenderDashboard';
+import Login from './pages/Login';
+
+function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/msme-dashboard" element={<MsmeDashboard />} />
+        <Route path="/lender-dashboard" element={<LenderDashboard />} />
+      </Routes>
+    </Router>
+  );
 }
+
+export default App;
