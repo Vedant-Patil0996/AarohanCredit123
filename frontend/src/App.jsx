85f78a8d7e220a8842e0b8e6f0dbc61457d9d13b
lone4alker <anoopshetty47@gmail.com>
2025-11-29 06:25:54 +0530

    landing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,14 +4,15 @@ import MsmeDashboard from './pages/MsmeDashboard';
 import LenderDashboard from './pages/LenderDashboard';
 import Login from './pages/Login';
 import LoanApplications from './pages/LoanApplications';
-
+import LandingPage from './pages/Landing';
 
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/msme-dashboard" element={<MsmeDashboard />} />
         <Route path="/lender-dashboard" element={<LenderDashboard />} />
         <Route path="/loan-applications" element={<LoanApplications />} />
